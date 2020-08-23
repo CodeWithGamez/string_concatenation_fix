a955d924ae2bf5d3a48690b4fbf13eb63841abfc
CodeWithGamez <codewithgamez@gmail.com>
2020-08-23 17:15:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+#Fix my mistakes of using STRING CONCATENATION
+
+
+#1 You won't get an error for this, but there should be spaces 
+
+words_1 = "This should"
+words_2 = "make a sentence"
+words_3 = "using string concatenation"
+print(words_1 + words_2 + words_3)
+
+
+#2
+
+words_4 = "This is lesson "
+number_1 = 5
+words_5 = " of chapter "
+number_2 = 1
+
+print( words_4 + number_1 + words_5 + number_2)
+
+#3 You won't get an error for this, but there should be spaces 
+
+words_6 = "Remember to check"
+words_7 = "previous lessons"
+words_8 = "or search to help whenever you get"
+words_9 = "because you will get stuck"
+
+print( words_6 + words_7 + words_8 + words_9)
